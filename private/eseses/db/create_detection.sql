bc4b69ca16d928e01ba7c892ce752e1ae649dc9e
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2025-03-18 11:52:01 -0700

    Create scripts for detection and model tables

---
+++ private/eseses/db/create_detection.sql
@@ -0,0 +1,32 @@
+CREATE SEQUENCE IF NOT EXISTS public.detection_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+CREATE TABLE IF NOT EXISTS public.detection
+(
+    id integer NOT NULL DEFAULT nextval('detection_id_seq'::regclass),
+    site_id text COLLATE pg_catalog."default" NOT NULL,
+    model_id integer,
+    label text COLLATE pg_catalog."default",
+    eventtype text COLLATE pg_catalog."default",
+    startdate time with time zone,
+    enddate time with time zone,
+    probability numeric,
+    CONSTRAINT detection_pkey PRIMARY KEY (id),
+    CONSTRAINT detection_model_fk FOREIGN KEY (model_id)
+        REFERENCES public.model (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT detection_site_fk FOREIGN KEY (site_id)
+        REFERENCES public.site (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.detection
+    OWNER to mmgis;
---
+++ private/eseses/db/create_model.sql
@@ -0,0 +1,19 @@
+CREATE SEQUENCE IF NOT EXISTS public.model_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+CREATE TABLE IF NOT EXISTS public.model
+(
+    id integer NOT NULL DEFAULT nextval('model_id_seq'::regclass),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    mode text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT model_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.model
+    OWNER to mmgis;
