f7d429d81d129f7d62676e806ba1e0f7eb251f59
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2025-05-13 17:53:11 -0700

    Allow filtering by mode and model with detections API

--- API/MMGIS-Private-Backend/MGViz/routes/mgviz.js
+++ API/MMGIS-Private-Backend/MGViz/routes/mgviz.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const { sequelize } = require("../../../connection");
 const logger = require("../../../logger");
 
+const isValidString = (str) => {
+  return str && typeof str === "string" && /^[a-zA-Z0-9_\-\.]+$/.test(str);
+};
+
 router.get("/coseismic", function (req, res, next) {
   coseismic(req, res, next, "get");
 });
@@ -112,10 +116,41 @@ function detections(req, res, next, type) {
     return;
   }
 
-  if (isISODateString(params.startdate) && isISODateString(params.enddate)) {
+  if (isISODateString(params.startdate) && isISODateString(params.enddate)
+    && isValidString(params.mode) && isValidString(params.model)) {
     sequelize
       .query(
-        "SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(json_build_object('type', 'Feature', 'geometry', ST_AsGeoJSON(geom)::json, 'properties', jsonb_strip_nulls(to_jsonb(detection_site) - 'geometry')))) FROM (SELECT site.id, detection_id, detection.label, eventtype, startdate, enddate, ST_GeomFROMText('POINT(' || cast(x as text)|| ' ' || cast(y as text) || ')', 4326) as geom FROM public.detection, site WHERE detection.site_id = site.id and probability is not null AND (startdate, enddate) OVERLAPS ($startdate, $enddate)) as detection_site;",
+        "SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(json_build_object('type', 'Feature', 'geometry', ST_AsGeoJSON(geom)::json, 'properties', jsonb_strip_nulls(to_jsonb(detection_site) - 'geometry')))) FROM (SELECT site.id, model.name, model.mode, detection_id, detection.label, eventtype, startdate, enddate, ST_GeomFROMText('POINT(' || cast(x as text)|| ' ' || cast(y as text) || ')', 4326) as geom FROM public.detection, site, model WHERE detection.site_id = site.id and probability is not null AND detection.model_id = model.id AND model.mode = $mode AND model.name = $model AND (startdate, enddate) OVERLAPS ($startdate, $enddate)) as detection_site;",
+        {
+          bind: {
+            startdate: params.startdate,
+            enddate: params.enddate,
+            mode: params.mode,
+            model: params.model,
+          },
+        }
+      )
+      .then(([detections]) => {
+        res.send(detections[0]["json_build_object"]);
+      })
+      .catch((err) => {
+        logger(
+          "error",
+          "SQL error while acquiring detections.",
+          req.originalUrl,
+          req,
+          err
+        );
+        res.send({
+          status: "failure",
+          message: "SQL error while acquiring detections.",
+          body: {},
+        });
+      });
+  } else if (isISODateString(params.startdate) && isISODateString(params.enddate)) {
+    sequelize
+      .query(
+        "SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(json_build_object('type', 'Feature', 'geometry', ST_AsGeoJSON(geom)::json, 'properties', jsonb_strip_nulls(to_jsonb(detection_site) - 'geometry')))) FROM (SELECT site.id, model.name, model.mode, detection_id, detection.label, eventtype, startdate, enddate, ST_GeomFROMText('POINT(' || cast(x as text)|| ' ' || cast(y as text) || ')', 4326) as geom FROM public.detection, site, model WHERE detection.site_id = site.id and probability is not null AND detection.model_id = model.id AND (startdate, enddate) OVERLAPS ($startdate, $enddate)) as detection_site;",
         {
           bind: {
             startdate: params.startdate,
@@ -143,7 +178,7 @@ function detections(req, res, next, type) {
   } else {
     sequelize
       .query(
-        "SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(json_build_object('type', 'Feature', 'geometry', ST_AsGeoJSON(geom)::json, 'properties', jsonb_strip_nulls(to_jsonb(detection_site) - 'geometry')))) FROM (SELECT site.id, detection_id, detection.label, eventtype, startdate, enddate, ST_GeomFROMText('POINT(' || cast(x as text)|| ' ' || cast(y as text) || ')', 4326) as geom FROM public.detection, site WHERE detection.site_id = site.id and probability is not null) as detection_site;",
+        "SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(json_build_object('type', 'Feature', 'geometry', ST_AsGeoJSON(geom)::json, 'properties', jsonb_strip_nulls(to_jsonb(detection_site) - 'geometry')))) FROM (SELECT site.id, model.name, model.mode, detection_id, detection.label, eventtype, startdate, enddate, ST_GeomFROMText('POINT(' || cast(x as text)|| ' ' || cast(y as text) || ')', 4326) as geom FROM public.detection, site, model WHERE detection.site_id = site.id and probability is not null AND detection.model_id = model.id) as detection_site;"
       )
       .then(([detections]) => {
         res.send(detections[0]["json_build_object"]);
@@ -161,7 +196,7 @@ function detections(req, res, next, type) {
           message: "SQL error while acquiring detections.",
           body: {},
         });
-      });    
+      });
   }
 }
 
